73202aa6f5cadefcb64d458e3e3864e7f117f2eb
不会打王者的菜鸡 <104179993+Zengga@users.noreply.github.com>
2023-09-04 23:02:30 +0800

    Update LoginApi.h
    升级了LoginApi

--- RainyApp/RainApp/LoginApi.h
+++ RainyApp/RainApp/LoginApi.h
@@ -4,36 +4,37 @@
 #include <stdexcept>
 #include <regex>
 
-/**
+/*
  * auth.LoginService
  */
-class Longin {
-public:
-	Longin() :
-		field_constraint(std::nullopt, std::nullopt, 1, 32, std::nullopt),
-		password_constraint(std::nullopt, std::nullopt, 6, 32, std::nullopt)
-	{}
-	virtual ~Longin() = default;
+class ApifoxModel {
+    public:
+    ApifoxModel() :
+        field_constraint(std::nullopt, std::nullopt, 1, 32, std::nullopt),
+        password_constraint(std::nullopt, std::nullopt, 6, 32, std::nullopt)
+    {}
+    virtual ~ApifoxModel() = default;
 
-private:
-	std::string field;
-	ClassMemberConstraints field_constraint;
-	std::shared_ptr<bool> is_with_api_key;
-	std::string password;
-	ClassMemberConstraints password_constraint;
+    private:
+    std::string field;
+    ClassMemberConstraints field_constraint;
+    std::shared_ptr<bool> is_with_api_key;
+    std::string password;
+    ClassMemberConstraints password_constraint;
 
-public:
-	const std::string & get_field() const { return field; }
-	std::string & get_mutable_field() { return field; }
-	void set_field(const std::string & value) { CheckConstraint("field", field_constraint, value); this->field = value; }
+    public:
+    const std::string & get_field() const { return field; }
+    std::string & get_mutable_field() { return field; }
+    void set_field(const std::string & value) { CheckConstraint("field", field_constraint, value); this->field = value; }
 
-	/**
-	 * ÊÇ·ñ´øAPIKey·µ»Ø
-	 */
-	std::shared_ptr<bool> get_is_with_api_key() const { return is_with_api_key; }
-	void set_is_with_api_key(std::shared_ptr<bool> value) { this->is_with_api_key = value; }
+    /*
+     * æ˜¯å¦å¸¦APIKeyè¿”å›
+     */
+    std::shared_ptr<bool> get_is_with_api_key() const { return is_with_api_key; }
+    void set_is_with_api_key(std::shared_ptr<bool> value) { this->is_with_api_key = value; }
 
-	const std::string & get_password() const { return password; }
-	std::string & get_mutable_password() { return password; }
-	void set_password(const std::string & value) { CheckConstraint("password", password_constraint, value); this->password = value; }
+    const std::string & get_password() const { return password; }
+    std::string & get_mutable_password() { return password; }
+    void set_password(const std::string & value) { CheckConstraint("password", password_constraint, value); this->password = value; }
 };
+
